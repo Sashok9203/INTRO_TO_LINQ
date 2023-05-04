2181076228479a2d2134488180b7fdcc7a35e7eb
Sashok9203 <56803757+Sashok9203@users.noreply.github.com>
2023-05-04 15:21:53 +0300

    Добавьте файлы проекта.

---
+++ INTRO_TO_LINQ/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace INTRO_TO_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
